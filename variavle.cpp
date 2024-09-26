91d6fc2ad3d1b718e8811a36bb3a9467214d6503
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-09-26 16:59:09 +0600

    Create variavle.cpp

---
+++ variavle.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    double  price = 99.99;
+    float  interestRate = 3.67f;
+    long fileSize = 90000L;
+    char letter = 'a';
+    bool isValid = false;
+
+    return 0;
+
+}
