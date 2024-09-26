dfe75ce5c8511c4720bfca3f8aa194d8875aec2f
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-09-26 16:36:31 +0600

    Create 3.cpp

---
+++ 3.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "Enter a values for x and y:";
+    double x;
+    double y;
+    cin >> x;
+    cin >> y;
+    cout << x + y;
+    return 0;
+}
