ee25fbbbdf83d464a6d02031dcbca66494bf8e32
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-10-01 12:59:18 +0600

    Create tyoedef.cpp

---
+++ tyoedef.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+
+typedef std::vector<std::pair<std::string,int>> pairlist_t;
+
+int main() {
+
+    pairlist_t pairlist;
+
+    return 0;
+
+
+}
+include <iostream>
+#include <vector>
+
+using namespace std;
+ //typedef std::vector<std::pair<std::string,int>> pairlist_t;
+typedef std::string text_t;
+typedef int number_t;
+
+int main() {
+
+    text_t  firstName = "rahat";
+    number_t  age = 19;
+    cout << firstName << '\n';
+    cout << age << '\n';
+
+    return 0;
+
+
+}
