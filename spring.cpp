0aa8d0537f1c694010f54d3226833d687ecaa568
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-10-01 12:42:01 +0600

    Create spring.cpp

---
+++ spring.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    string  name = "rahat";
+    string  day = "tuesday";
+    string month = "october";
+    cout << name;
+
+    return 0;
+
+}
