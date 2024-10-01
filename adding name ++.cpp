9d0966ca2013280fd86e29bd02a79ac977a90412
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-10-01 12:45:16 +0600

    Create adding name ++.cpp

---
+++ adding name ++.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    string  name = "rahat";
+    string  day = "tuesday";
+    string month = "october";
+    cout << "Hello " << name << '\n';
+    cout << "its  " << day << '\n';
+    
+
+    return 0;
+
+}
