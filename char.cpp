f72bac2c76f4a1ed41b39279cc9da13ed7dd3c6b
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-10-01 12:37:47 +0600

    Create char.cpp

---
+++ char.cpp
@@ -0,0 +1,14 @@
+nclude <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using namespace std;
+
+int main() {
+    char grade = 'A';
+    char  initial = 'c';
+    cout << initial;
+
+    return 0;
+
+}
