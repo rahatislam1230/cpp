424552f6f3b95b87cf259adefa912db0be5c9c4f
Rahad-islam1230 <rahatislam2100@gmail.com>
2024-10-03 20:33:28 +0600

    Create switch.cpp

---
+++ switch.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    int month;
+    cout << "Enter the month (1-12): ";
+    cin >> month;
+
+    switch (month) {
+        case 1:
+            cout << "It's jan";
+            break;
+        case 2:
+            cout << "its fab";
+            break;
+        case 3:
+            cout << "its march";
+            break;
+        case 4:
+            cout << "its apirl";
+            break;
+            //case 12
+    }
+
+    return 0;
+
+
+}
