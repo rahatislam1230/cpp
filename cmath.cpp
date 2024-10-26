4d1a637c31c7f548301157449e53b0d3c6736ada
Rahad Islam <rahatislam2100@gmail.com>
2024-10-26 18:54:28 +0600

    Create cmath.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ cmath.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main(){
+    double x = 3.14;
+    double y =4;
+    double z;
+    //z = max(x,y);
+    //z = min(x,y);
+    //z = pow(2,4);
+    //z = sqrt(9);
+    //z = abs(-3);
+    //z = round(x);
+    //z = ceil(x);
+    z = floor(x);
+    cout<<z;
+    return 0;
+
+}
