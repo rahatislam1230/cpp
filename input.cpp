d2b575913573053e43103f70115a812e75b27791
Rahad Islam <rahatislam2100@gmail.com>
2024-10-26 11:48:27 +0600

    Create input.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ input.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    cout<<"Enter your name : ";
+    string name;
+    cin>>name;
+    cout<<"Enter your age: ";
+    string age;
+    cin>>age;
+    cout<<"what do your like to eat?: ";
+    string food;
+    cin>>food;
+    cout<<"your name is "<<name<<"\n";
+    cout<<"your are "<<age<<" years old "<<"\n";
+    cout<<"your fav food is "<<food<<" right?"<<"\n";
+
+
+    return 0;
+
+}
