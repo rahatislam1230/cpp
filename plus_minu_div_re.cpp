5655a78215d16de12edf98ccffc16dfd7aac983d
Rahad Islam <rahatislam2100@gmail.com>
2024-10-26 11:58:15 +0600

    Create plus_minu_div_re.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ plus_minu_div_re.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int student = 20;
+    //student +=2;
+    //student+=1;
+    //student++;
+    //student -=2;
+    //student-=1;
+    //student--;
+    //student = student * 2;
+    //student*=2;
+    //student = student / 2;
+    //student/=2;
+    int remainder = student % 3;
+
+    cout<<remainder;
+
+
+    return 0;
+
+}
