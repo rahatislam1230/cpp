905711582ed692b4c0c02cb51036262e4be002bf
Rahad Islam <rahatislam2100@gmail.com>
2024-11-16 21:26:00 +0600

    Create extentloop.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ extentloop.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int i = 5;
+    while(true){
+        if(i%7==0){
+            cout<<i<<endl;
+            break;
+        }
+        i+=5;
+    }
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int n;
+    cin>>n;
+    int sum =0;
+    do{
+        int sum;
+        cin>>n;
+        sum>>n;
+        n --;
+    }while(n>0);
+        cout<<sum<<endl;
+    return 0;
+}
