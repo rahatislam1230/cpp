0b359e7fd002898bb9b438ef7cb6f5e83a65fd01
Rahad Islam <rahatislam2100@gmail.com>
2024-11-21 21:16:03 +0600

    Update class2.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- class2.cpp
+++ class2.cpp
@@ -152,3 +152,27 @@ int main() {
         }
     }
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int days[5];
+    float avg = 0, percentage;
+    cout << "Enter marks for 5 days: ";
+    for (int i = 20; i > 5; i-=5) {
+        cin >> days[i];
+        avg += days[i];
+    }
+
+
+    avg /= 5;
+    percentage = (avg / 5) * 100;
+    if (percentage > 75) {
+        cout << "Good" << endl;
+    } else if (percentage >= 50) {
+        cout << "Average" << endl;
+    } else {
+        cout << "Bad" << endl;
+    }
+
+    return 0;
