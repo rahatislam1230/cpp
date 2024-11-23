24887d5510e7c603d97c3c714576a51f14b972e1
Rahad Islam <rahatislam2100@gmail.com>
2024-11-23 19:51:40 +0600

    Update leapyear.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- leapyear.cpp
+++ leapyear.cpp
@@ -14,3 +14,20 @@ int main()
 
     return 0;
 }
+#include <iostream>
+using namespace std;
+int main() {
+    int year,rem_4,rem_100,rem_400;
+    cout<<"enter a year: "<<endl;
+    cin>>year;
+    rem_4=year%4;
+    rem_100=year%100;
+    rem_400=year%400;
+    if( (rem_4==0 && rem_100!=0)||rem_400==0 ){
+        cout<<"it's a leap year "<<endl;
+    }else{
+        cout<<"it's not a leap year "<<endl;
+    }
+
+
+}
