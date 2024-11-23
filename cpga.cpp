4df6a0f2380ced7fb67e3af69efc562bdcc91562
Rahad Islam <rahatislam2100@gmail.com>
2024-11-23 21:14:45 +0600

    Update cpga.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- cpga.cpp
+++ cpga.cpp
@@ -38,3 +38,36 @@ int main() {
 
 
 }
+#include <iostream>
+using namespace std;
+int main() {
+    float phy,che,it,math;
+    int n,g,f,m;
+    float cpga,sum=0;
+    cout<<"Enter cpga of physics & credit:"<<endl;
+    cin>>phy>>n;
+    cout<<"Enter cpga of chemistry & credit:"<<endl;
+    cin>>che>>m;
+    cout<<"Enter cpga of It & credit:"<<endl;
+    cin>>it>>f;
+    cout<<"Enter cpga of math & credit:"<<endl;
+    cin>>math>>g;
+
+    sum=(phy*n)+(che*m)+(it*f)+(math*g);
+    cpga=sum/12;
+    if(cpga>3.7){
+        cout<<"good"<<endl;
+    }else if(cpga>3.5){
+        cout<<"not bad"<<endl;
+    }else if(cpga>3.0){
+        cout<<"need to improve"<<endl;
+    }
+    else{
+
+        cout<<"poor"<<endl;
+
+
+    }
+
+
+}
