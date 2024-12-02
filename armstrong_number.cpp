4a8b67d95551ee469973d4dd8805f0223818f915
Rahad Islam <rahatislam2100@gmail.com>
2024-12-02 17:53:33 +0600

    Update armstrong_number.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- armstrong_number.cpp
+++ armstrong_number.cpp
@@ -89,4 +89,110 @@ int main() {
 
     return 0;
 }
+#include <iostream>
+using namespace std;
 
+int main() {
+    const int DAYS = 7; 
+    float temperatures[DAYS]; 
+    float sum = 0.0, average;
+
+    cout << "Welcome to Weekly Temperature Analysis System!" << endl;
+    cout << "Please enter the daily temperatures for the week:" << endl;
+
+    for (int i = 0; i < DAYS; i++) {
+        cout << "Day " << i + 1 << ": ";
+        cin >> temperatures[i];
+        sum += temperatures[i];
+    }
+
+    average = sum / DAYS;
+
+
+    cout << "Average temperature per day: " << average << endl;
+
+    if (average >= 32) {
+        cout << "Hot Weather" << endl;
+    } else if (average >= 18 && average < 32) {
+        cout << "Moderate Weather" << endl;
+    } else {
+        cout << "Cold Weather" << endl;
+    }
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int const day=7;
+    float temperature[day];
+    float sum=0,average;
+    for (int i = 0; i < day; i++) {
+        cout<<"Enter the temperature of each days ["<<i<<"] : "<<endl;
+        cin>>temperature[i];
+        sum+=temperature[i];
+    }
+    average=sum/day;
+    cout<<"the average of a week is: "<<average<<endl;
+    if(average>=32){
+        cout<<"hot weather "<<endl;
+    }else if(average>=18 && average<32){
+        cout<<"Moderator weather "<<endl;
+    }else{
+        cout<<"cold weather "<<endl;
+    }
+
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout<<"Enter a value of array : "<<endl;
+    cin>>n;
+    float arry[n];
+    int sum=0;
+    for(int i =0;i<n;i++){
+        cout<<"Enter the value of a number ["<<i<<"] : "<<endl;
+        cin>>arry[i];
+        sum+=arry[i];
+    }
+    cout<<"the sum is :"<<sum<<endl;
+    if(sum%2== 0){
+        cout<<"the sum is a Even"<<endl;
+    }else{
+        cout<<"the sum is Odd "<<endl;
+    }
+
+
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int totalclass, totalattendance;
+    float percentage;
+    cout << "Enter the total classes held: ";
+    cin >> totalclass;
+    cout << "Enter total attendance: ";
+    cin >> totalattendance;
+    percentage = (float)totalattendance / totalclass * 100;
+    cout << "Attendance Percentage: " << percentage << "%" << endl;
+
+    if (percentage >= 80) {
+        cout << "Allowed to sit for the exam." << endl;
+
+        if (percentage == 100) {
+            cout << "You will get 2 bonus marks!" << endl;
+        }
+    } else {
+        cout << "Not allowed to sit for the exam." << endl;
+    }
+
+    return 0;
+}
