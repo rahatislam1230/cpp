157c16e6b857374a47f82e9d7b7a9748eeb29546
Rahad Islam <rahatislam2100@gmail.com>
2024-12-02 16:56:08 +0600

    Update armstrong_number.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- armstrong_number.cpp
+++ armstrong_number.cpp
@@ -33,4 +33,60 @@ int main()
 
     return 0;
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int totalClasses, attendedClasses;
+    cout << "Enter the total number of classes held: ";
+    cin >> totalClasses;
+
+    cout << "Enter the number of classes attended: ";
+    cin >> attendedClasses;
+
+    float attendancePercentage = (float)attendedClasses / totalClasses * 100;
+
+    cout << "Attendance Percentage: " << attendancePercentage << "%" << endl;
+
+    if (attendancePercentage < 80) {
+        cout << "Not Allowed" << endl;
+    } else {
+        cout << "Allowed" << endl;
+        if (attendancePercentage == 100) {
+            cout << "You get 2 bonus marks!" << endl;
+        }
+    }
+
+    return 0;
+}
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+
+    int arr[n];
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        if (i % 2 == 0) 
+            sum += arr[i];
+        }
+    }
+
+    cout << "Sum of elements at odd positions: " << sum << endl;
+    if (sum % 2 == 0) {
+        cout << "The summation is EVEN" << endl;
+    } else {
+        cout << "The summation is ODD" << endl;
+    }
+
+    return 0;
+}
 
