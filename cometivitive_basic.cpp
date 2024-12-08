6e0d5ef468470cfc6b0e34b0fffffba32b3fdd91
Rahad Islam <rahatislam2100@gmail.com>
2024-12-08 17:58:44 +0600

    Update and rename cometivitive.cpp to cometivitive_basic.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ cometivitive_basic.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a=100000;
+    int b=100000;
+    int c=a*b;
+    cout<<c<<endl;
+    int max = INT_MAX;
+    cout<<max+1;
+    return 0;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long int a=10000;
+    long int b=10000;
+    long int c=a*b;
+    cout<<c<<endl;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a=100000;
+    int b=100000;
+    long long int c=a*1LL *b;
+    cout<<c<<endl;
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    double a=100000;
+    double b=100000;
+    double c=a*b;
+    c=1E24;
+    cout<<fixed<<setprecision(0)<<c<<endl;
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    long long int l,b;
+    cin>>l>>b;
+    cout<<l*b<<endl;
+}
+
+
