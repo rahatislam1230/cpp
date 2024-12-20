882f020f22ceaa3a869ea19c609c797d6949c9de
Rahad Islam <rahatislam2100@gmail.com>
2024-12-20 21:31:03 +0600

    Update matrix.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- matrix.cpp
+++ matrix.cpp
@@ -230,4 +230,50 @@ int main(){
         cout<<"Element "<<x<<" not found in the matrix."<<endl;
     }
 }
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n1, n2, n3;
+    cin >> n1 >> n2 >> n3;
+
+    int m1[n1][n2];
+    int m2[n2][n3];
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n2; j++) {
+            cin >> m1[i][j];
+        }
+    }
+
+    for (int i = 0; i < n2; i++) {
+        for (int j = 0; j < n3; j++) {
+            cin >> m2[i][j];
+        }
+    }
+
+    int ans[n1][n3];
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n3; j++) {
+            ans[i][j] = 0;
+        }
+    }
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n3; j++) {
+            for (int k = 0; k < n2; k++) {
+                ans[i][j] += m1[i][k] * m2[k][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < n1; i++) {
+        for (int j = 0; j < n3; j++) {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
 
