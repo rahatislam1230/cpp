18dec55d2512998bb72679f0a0b167da7f95ec2c
Rahad Islam <rahatislam2100@gmail.com>
2024-12-25 23:14:40 +0600

    Create funtion.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

---
+++ funtion.cpp
@@ -0,0 +1,160 @@
+#include <bits/stdc++.h>
+using namespace std;
+bool isPrime(int num){
+    for(int i=2;i< sqrt(num);i++){
+        if(num%2==0){
+            return false;
+        }else{
+            return true;
+        }
+    }
+}
+int main() {
+    int a,b;
+    cin>>a>>b;
+    for(int i=a;i<b;i++){
+        if(isPrime(i)==true){
+            cout<<i<<endl;
+        }
+    }
+}
+#include <bits/stdc++.h>
+using namespace std;
+void fib(int n){
+    int t1=0;
+    int t2=1;
+    int next_term;
+    for(int i=1;i<n;i++){
+        cout<<t1<<endl;
+        next_term= t1+t2;
+        t1=t2;
+        t2=next_term;
+    }
+    return;
+}
+int main(){
+    int n;
+    cin>>n;
+    fib(n);
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    int factorial=1;
+    for(int i=2;i<n;i++){
+        factorial*=i;
+    }
+    return factorial;
+}
+int main(){
+   int n;
+   cin>>n;
+   int ans = fact(n);
+   cout<<ans<<endl;
+    return 0;
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    int factorial=1;
+    for(int i=2;i<n;i++){
+        factorial*=i;
+    }
+    return factorial;
+}
+int main(){
+   int n,r;
+   cin>>n>>r;
+   int ans = fact(n)/(fact(r)* fact(n-r));
+   cout<<ans<<endl;
+    return 0;
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+int fact(int n){
+    int factorial=1;
+    for(int i=2;i<=n;++i){
+        factorial*=i;
+    }
+    return factorial;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  for(int i=0;i<n;i++){
+      for(int j=0;j<=i;j++){
+          cout<<fact(i)/(fact(j)*fact(i-j))<<" ";
+      }cout<<endl;
+  }
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int n) {
+    int ans = 0;
+    for (int i = 1; i < n; i++) {
+        ans += 1;
+    }
+    return ans;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<sum(n)<<endl;
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+int sum(int n) {
+    int ans = 0;
+    for (int i = 1; i <=n; i++) {
+        ans += 1;
+    }
+    return ans;
+}
+
+int main(){
+  int n;
+  cin>>n;
+  cout<<sum(n)<<endl;
+
+}
+#include <bits/stdc++.h>
+using namespace std;
+
+bool check(int x, int y, int z) {
+    int a = max(x, max(y, z));
+    int b, c;
+
+    if (a == x) {
+        b = y;
+        c = z;
+    } else if (a == y) {
+        b = x;
+        c = z;
+    } else {
+        b = x;
+        c = y;
+    }
+
+    if (a * a == b * b + c * c) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+int main() {
+    int x, y, z;
+    cin >> x >> y >> z;
+    if (check(x, y, z)) {
+        cout << "pythagorean triple" << endl;
+    } else {
+        cout << "not a pythagorean triple" << endl;
+    }
+}
