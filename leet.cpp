de5f52df475198c1247f3a877f54f85c30b7f485
Rahad Islam <rahatislam2100@gmail.com>
2024-12-27 22:41:54 +0600

    Update leet.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- leet.cpp
+++ leet.cpp
@@ -126,3 +126,44 @@ public:
         return x == reversedHalf || x == reversedHalf / 10;
     }
 };
+class Solution {
+public:
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        int n = nums.size();
+
+        // Sort the array
+        sort(nums.begin(), nums.end());
+
+        // Iterate through the array
+        for (int i = 0; i < n - 2; ++i) {
+            // Skip duplicates for the first number
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+
+            int target = -nums[i]; // We need to find two numbers that sum up to the target
+            
+            // Use two pointers to find the remaining two numbers
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    // Skip duplicates for the second number
+                    while (left < right && nums[left] == nums[left + 1]) ++left;
+                    // Skip duplicates for the third number
+                    while (left < right && nums[right] == nums[right - 1]) --right;
+
+                    ++left;
+                    --right;
+                } else if (sum < target) {
+                    ++left;  // Increase the sum by moving the left pointer
+                } else {
+                    --right; // Decrease the sum by moving the right pointer
+                }
+            }
+        }
+        
+        return result;
+    }
+};
+
