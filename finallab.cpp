fa1ab32bd03bf0bf0cc3f38afc15e6194ef2c9f6
Rahad Islam <rahatislam2100@gmail.com>
2025-01-17 16:30:14 +0600

    Update finallab.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- finallab.cpp
+++ finallab.cpp
@@ -377,3 +377,43 @@ int main(){
 
 
 }
+#include <iostream>
+using namespace std;
+class Rectangle{
+private:
+    double length;
+    double width;
+public:
+    void setdimesions(double l,double w){
+        if(l>0 && w>0){
+            length=l;
+            width=w;
+        }else{
+            cout<<"Invalid dimesions"<<endl;
+        }
+    }
+    double calcuateArea(){
+            return length*width;
+        }
+    double calcuatePerimeter(){
+            return 2*(length+width);
+        }
+
+    void display(){
+        cout<<"length:"<<length<<endl;
+        cout<<"Width:"<<width<<endl;
+        cout<<"Area: "<< calcuateArea()<<endl;
+        cout<<"Perimeter:"<<calcuatePerimeter()<<endl;
+    }
+};
+
+int main(){
+    Rectangle rec;
+    double length,width;
+    cout<<"Enter the length of Rectangle:";
+    cin>>length;
+    cout<<"Enter the width Rectangle:";
+    cin>>width;
+    rec.setdimesions(length,width);
+    rec.display();
+}
