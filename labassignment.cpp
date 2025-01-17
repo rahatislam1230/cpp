837fc60de1a0586dd6206d969cfe91efda668ef8
Rahad Islam <rahatislam2100@gmail.com>
2025-01-17 21:53:18 +0600

    Update labassignment.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- labassignment.cpp
+++ labassignment.cpp
@@ -296,7 +296,32 @@ int main(){
 
 
 }
+8..........
+#include <iostream>
+using namespace std;
+int arraySum(int arr[], int size) {
+    int sum = 0;
+    for (int i = 0; i < size; ++i) {
+        sum += arr[i];
+    }
+    return sum;
+}
+int main() {
+    int n;
+    cout << "Enter the number of elements in the array: ";
+    cin >> n;
+    int arr[n];
+    cout << "Enter the elements of the array:" << endl;
+    for (int i = 0; i < n; ++i) {
+        cin >> arr[i];
+    }
+    int totalSum = arraySum(arr, n);
+    cout << "The sum of the elements in the array is: " << totalSum << endl;
 
+    return 0;
+}
+    
+    
 
 
 
