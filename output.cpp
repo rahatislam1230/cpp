fafbbd57af7b38d6890d6011f12b68c9f4c7d4c0
Rahad Islam <rahatislam2100@gmail.com>
2025-01-29 16:22:16 +0600

    Update output.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- output.cpp
+++ output.cpp
@@ -109,3 +109,30 @@ int main() {
     return 0;
 }
 //1 5 3
+#include <iostream>
+using namespace std;
+
+int main() {
+    int matrix[3][3] = {
+            {2, 5, 3},
+            {2, 5, 6},
+            {4, 8, 9}
+    };
+
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 3; j++) {
+
+            if (matrix[i][j] % 2 == 0) {
+                break; // Exit inner loop if an even number is found
+            }
+
+            cout << matrix[i][j] << " ";
+
+            continue; // Skip the rest of the loop (not needed here but included for example)
+        }
+        cout << endl; // Move to the next row
+    }
+
+    return 0;
+}
+// no output
