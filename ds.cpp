f9050e6a6f4b348880be2ff66609c7790949a0c3
Rahad Islam <rahatislam2100@gmail.com>
2025-07-26 18:08:20 +0600

    Update ds.cpp
    Signed-off-by: Rahad Islam <rahatislam2100@gmail.com>

--- ds.cpp
+++ ds.cpp
@@ -260,3 +260,228 @@ int main(){
 
     cout << "Duplicate count: " << count << endl;
 }
+#include<iostream>
+using namespace std;
+struct Book{
+    int id;
+    string title;
+    float price;
+}a[3];
+int main(){
+    for(int i=0;i<3;i++){
+        cin>>a[i].id>>a[i].title>>a[i].price;
+    }
+    for(int i=0;i<3;i++){
+        cout<<a[i].id<<a[i].title<<a[i].price;
+    }
+
+}
+
+#include<iostream>
+using namespace std;
+struct Book{
+    int id;
+    string title;
+    float price;
+}a[3];
+int main(){
+    for(int i=0;i<3;i++){
+        cin>>a[i].id>>a[i].title>>a[i].price;
+    }
+    for(int i=0;i<3;i++){
+       if(a[i].price>50000){
+        cout<<"name:"<<a[i].id;
+       }
+    }
+
+}
+
+#include<iostream>
+using namespace std;
+bool isPrime(int n){
+        if(n<=1){
+            return false;
+        }for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+int main(){
+   int arr[10] = {2, 3, 4, 5, 10, 13, 17, 20, 29, 31};
+   for(int i=0;i<10;i++){
+    if(isPrime(arr[i])){
+        cout<<arr[i]<<"prime"<<endl;
+    }else{
+      cout<<arr[i]<<"not prime"<<endl;
+    }
+   }
+
+}
+
+#include<iostream>
+using namespace std;
+
+int main(){
+   int arr[5] = {2, 3, 4, 5, 10};
+   int *ptr=arr;
+   for(int i=4;i>=0;i--){
+    cout<<*(ptr+i)<<" ";
+   }
+
+}
+#include<iostream>
+using namespace std;
+void change(int &a,int &b){
+      int temp=a;
+      a=b;
+      b=temp;
+}
+
+int main(){
+   int a=10,b=50;
+   cout<<a<<" "<<b;
+    change(a,b);
+
+    cout<<a<<" "<<b;
+}
+
+#include<iostream>
+using namespace std;
+void change(int &a,int &b){
+      int temp=a;
+}
+
+int main(){
+   int arr[6] = {2, 3, 4, 5, 10,6};
+   int *ptr=arr;
+   for(int i=0;i<6;i++){
+    if(i%2!=0){
+        cout<<*(ptr+i)<<"odd"<<endl;
+    }
+   }
+
+
+}
+#include<iostream>
+using namespace std;
+bool isPrime(int n){
+        if(n<=1){
+            return false;
+        }for(int i=2;i<n;i++){
+            if(n%i==0){
+                return false;
+            }
+        }
+        return true;
+    }
+
+int countP(int *arr){
+    int cot=0;
+    for(int i=0;i<10;i++){
+        if(isPrime(*(arr+i))){
+            cot++;
+        }
+    }
+    return cot;
+}
+int main(){
+   int arr[10] = {2, 3, 4, 5, 10, 13, 17, 20, 29, 31};
+   for(int i=0;i<10;i++){
+    if(isPrime(arr[i])){
+        cout<<arr[i]<<"prime"<<endl;
+    }else{
+      cout<<arr[i]<<"not prime"<<endl;
+    }
+   }
+int p=countP(arr);
+cout<<p<<endl;
+cout<<p-arr[10];
+}
+#include<iostream>
+using namespace std;
+
+int main(){
+   int arr[10] = {2, 3, 4, 5, 10, 13, 17, 20, 29, 31};
+   int x=10;
+   bool flag=false;
+   for(int i=0;i<10;i++){
+    if(arr[i]==x){
+        cout<<"found!["<<i<<"]";
+        flag=true;
+        break;
+    }
+   }
+   if(!flag){
+    cout<<" not found!";
+   }
+}
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {2, 3, 4, 5, 10, 13, 17, 20, 29, 31};
+    int x = 10;
+    int count = 0;
+    for(int i = 0; i < 10; i++) {
+        if(arr[i] == x) {
+            count++;
+        }
+    }
+
+    if(count > 0) {
+        cout << "Found " << count << " times!";
+    } else {
+        cout << "Not found!";
+    }
+
+    return 0;
+}
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {2, 3, 4, 5, 10, 13, 10, 20, 29, 31};
+    cout<<"first"<<endl;
+    for(int p=0;p<10;p++){
+        cout<<arr[p]<<" ";
+    }
+    for(int j=0;j<10;j++){
+    for(int i=0;i<9;i++){
+        if(arr[i]<arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+}
+   cout<<"after"<<endl;
+    for(int p=0;p<10;p++){
+        cout<<arr[p]<<" ";
+    }
+}
+#include<iostream>
+using namespace std;
+
+int main() {
+    int arr[10] = {2, -3, -4, 55, 10, 13, -10, 20, 29, 31};
+    cout<<"first"<<endl;
+    for(int p=0;p<10;p++){
+        cout<<arr[p]<<" ";
+    }
+    for(int j=0;j<10;j++){
+    for(int i=0;i<9;i++){
+        if(arr[i]>arr[i+1]){
+            int temp=arr[i];
+            arr[i]=arr[i+1];
+            arr[i+1]=temp;
+        }
+    }
+}
+   cout<<"after"<<endl;
+    for(int p=0;p<10;p++){
+        cout<<arr[p]<<" ";
+    }
+}
